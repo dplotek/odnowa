c18d69d21d340d1edd1d52d861bed7bc36d29de9
Dawid Płotek <88239345+dplotek@users.noreply.github.com>
2022-11-22 18:42:09 +0100

    feat: fonts implementation (#2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+        'oswald': ["Oswald", 'sans-serif']
+      }
+    },
   },
   plugins: [
     require('tailwindcss'),
