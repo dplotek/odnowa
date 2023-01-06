3910dc6e2415805d56ec2981ba65da5e40db1b02
Dawid Płotek <88239345+dplotek@users.noreply.github.com>
2023-01-06 14:12:24 +0100

    Footer (#4)
    * footer
    
    * final footer
    
    * feat: nav and footer applied on all pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
         'lato': ['Lato', 'sans-serif'],
         'oswald': ["Oswald", 'sans-serif'],
         'body': ['Lato', 'sans-serif'],
+      },
+      colors: {
+        secondary: "#e1ad00"
+      },
+      textColor: {
+        secondary: "#e1ad00"
       }
     },
   },
